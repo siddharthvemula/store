393d768fa0f0ee1bf9e0f459f439a6f74df80455
siddharth.vemula <siddharth.vemula@gmail.com>
2024-07-15 22:30:54 -0400

    Cart functionality Incomplete

--- src/main/java/com/sid/store/controller/HomeController.java
+++ src/main/java/com/sid/store/controller/HomeController.java
@@ -1,8 +1,15 @@
 package com.sid.store.controller;
 
+import com.sid.store.model.Cart;
 import com.sid.store.model.Item;
+import com.sid.store.model.Temp;
+import com.sid.store.model.User;
+import com.sid.store.persistence.CartRepository;
 import com.sid.store.persistence.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +21,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HomeController {
     @Autowired
     ItemRepository itemRepository;
+
+    @Autowired
+    CartRepository cartRepository;
 
     @GetMapping("/")
     public String home(Model model) {
@@ -28,4 +38,14 @@ public class HomeController {
         model.addAttribute("item", item);
         return "home/product";
     }
+    @PostMapping("/cart")
+    public String addToCart(@AuthenticationPrincipal User user, @ModelAttribute Cart cart, Model model) {
+        //Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        //cart.setUserId((((User)authentication.getPrincipal()).getId()));
+        cart.setUserId(user.getId());
+        cartRepository.save(cart);
+        return "redirect:/";
+    }
 }
+
+
